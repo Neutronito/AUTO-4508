0aa227a3aa67d9c84e6b1ebdc002e8da5cd1c0f0
GROUP2 <agni.gango@gmail.com>
2023-05-08 17:17:55 +0800

    Created imu publisher, added imu publisher and gps publisher to launch file and modified waypoint driver srv

--- catkin_ws/src/imu_reader/scripts/imu_publisher_node.py
+++ catkin_ws/src/imu_reader/scripts/imu_publisher_node.py
@@ -1,48 +1,58 @@
+import rospy
+from std_msgs.msg import Float64
 from Phidget22.Phidget import *
 from Phidget22.Devices.Spatial import *
 import time
 
+imu_publisher_handle = None
+spatial_channel = None
+
 #Declare any event handlers here. These will be called every time the associated event occurs.
 
 def onAlgorithmData(self, quaternion, timestamp):
-	print("Timestamp: " + str(timestamp))
-
+	global imu_publisher_handle
+	
 	eulerAngles = self.getEulerAngles()
-	print("EulerAngles: ")
-	print("\tpitch: " + str(eulerAngles.pitch))
-	print("\troll: " + str(eulerAngles.roll))
-	print("\theading: " + str(eulerAngles.heading))
+	rospy.loginfo("Broadcasting a heading of " + str(eulerAngles.heading))
+	imu_publisher_handle.publish(eulerAngles.heading)
+	
 
-	quaternion = self.getQuaternion()
-	print("Quaternion: ")
-	print("\tx: " + str(quaternion.x))
-	print("\ty: " + str(quaternion.y))
-	print("\tz: " + str(quaternion.z))
-	print("\tw: " + str(quaternion.w))
-	print("----------")
 
 def main():
+	global imu_publisher_handle
+	global spatial_channel
+
+	# Now start the publisher
+	imu_publisher_handle = rospy.Publisher('imu_heading', Float64, queue_size=10)
+	rospy.init_node('imu_publisher', anonymous=True)
+	
 	#Create your Phidget channels
-	spatial0 = Spatial()
+	spatial_channel = Spatial()
 
 	#Set addressing parameters to specify which channel to open (if any)
-	spatial0.setDeviceSerialNumber(373542)
+	spatial_channel.setDeviceSerialNumber(373542)
 
-	#Assign any event handlers you need before calling open so that no events are missed.
-	spatial0.setOnAlgorithmDataHandler(onAlgorithmData)
+	#Assign any event handlers you need before calling open so that no events are missed
+	spatial_channel.setOnAlgorithmDataHandler(onAlgorithmData)
 
 	#Open your Phidgets and wait for attachment
-	spatial0.openWaitForAttachment(5000)
+	spatial_channel.openWaitForAttachment(5000)
 
-	#Do stuff with your Phidgets here or in your event handlers.
-	spatial0.setHeatingEnabled(True)
+	#Turn on heating
+	spatial_channel.setHeatingEnabled(True)
 
-	try:
-		input("Press Enter to Stop\n")
-	except (Exception, KeyboardInterrupt):
-		pass
+	# Attach the shutdown handler
+	rospy.on_shutdown(shutdown_handler)
+    
+	rospy.loginfo("IMU Publisher node has successfully setup the IMU")
 
-	#Close your Phidgets once the program is done.
-	spatial0.close()
+	rospy.spin()
+
+def shutdown_handler():
+	global spatial_channel
+
+	# Close the phidget channel
+	rospy.loginfo("Node kill recieved, shutting down phidget channel")
+	spatial_channel.close()
 
 main()
--- catkin_ws/src/waypoint_driver/src/waypoint_driver_node.cpp
+++ catkin_ws/src/waypoint_driver/src/waypoint_driver_node.cpp
@@ -1,23 +1,58 @@
 #include "ros/ros.h"
 #include "waypoint_driver/gps_points.h"
+#include "std_msgs/Float64.h"
+#include "sensor_msgs/NavSatFix.h"
+#include <stdlib.h>
+
+// THIS CODE IS STUPID
+// Someone else tried to do this, this is how you would do it:
+// https://answers.ros.org/question/339442/is-it-possible-to-have-a-subscriber-inside-the-service-server/
+
+void gps_value_callback(const sensor_msgs::NavSatFix::ConstPtr& fix) {
+	ROS_INFO("Recieved a navsat message, longitude is %f and latitude is %f", 180.0, 180.0);
+}
+
+void imu_value_callback(const std_msgs::Float64::ConstPtr& heading) {
+	ROS_INFO("Recieved a heading, value is %f", 180.0);
+
+}
 
 bool drive_waypoint(waypoint_driver::gps_points::Request  &req, waypoint_driver::gps_points::Response &res) {
-  double latitude = req.latitude;
-  double longitude = req.latitude;
-  ROS_INFO("Recieved request of: lat=%f, long=%f", latitude, longitude);
-  bool returnval = false;
-  ROS_INFO("sending back response: [%d]", returnval);
-  res.reached_status = returnval;
-  return true;
+	// Parse recieved data
+	double latitude = req.latitude;
+	double longitude = req.latitude;
+	ROS_INFO("Recieved request of: lat=%f, long=%f", latitude, longitude);
+	bool returnval = false;
+
+	// Start the gps_waypoint reader
+	ros::Subscriber gps_subscriber = waypoint_handle.subscribe("fix", 10, gps_value_callback);
+
+	// Start the imu reader
+	ros::Subscriber imu_subscriber = waypoint_handle.subscribe("imu_heading", 10, imu_value_callback);
+
+	while(true) {
+
+	}
+
+	// Send back the response
+	ROS_INFO("sending back response: [%d]", returnval);
+	res.reached_status = returnval;
+	
+	return true;
 }
 
 int main(int argc, char **argv) {
-  ros::init(argc, argv, "waypoint_driver");
-  ros::NodeHandle waypoint_handle;
+	ros::init(argc, argv, "waypoint_driver");
+	ros::start();
 
-  ros::ServiceServer service = waypoint_handle.advertiseService("gps_points", drive_waypoint);
-  ROS_INFO("Ready to add two ints.");
-  ros::spin();
+	ros::NodeHandle waypoint_handle;
 
-  return 0;
+	// start the way point driving service
+	// Boost is being used here to pass in the waypoint handle to the drive waypoin srv callback
+	ros::ServiceServer service = waypoint_handle.advertiseService("gps_points",
+		boost::bind(&drive_waypoint, boost::ref(waypoint_handle), _1));
+	ROS_INFO("Ready to add two ints.");
+	ros::spin();
+
+	return 0;
 }
