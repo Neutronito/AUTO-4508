c8c1e61b6ba5c69f86d81908f9bc78c50cef226c
Neutronito <vg.sinalco@gmail.com>
2023-05-13 14:57:05 +0800

    Implemented dead man publishing

--- catkin_ws/src/drive_values/src/remote_robot_driving.cpp
+++ catkin_ws/src/drive_values/src/remote_robot_driving.cpp
@@ -2,6 +2,10 @@
 #include <geometry_msgs/Twist.h>
 #include <sensor_msgs/Joy.h>
 #include <global_controller/controller_states.h>
+#include "drive_values/enable_remote.h"
+#include <std_msgs/Bool.h>
+
+bool driving_enabled  = true;
 
 class RemoteDrive
 {
@@ -11,17 +15,26 @@ public:
 private:
   void joyCallback(const sensor_msgs::Joy::ConstPtr& joy);
   void updateState(const global_controller::controller_states::ConstPtr& states);
+//   bool triggered_driving_enabled_change(drive_values::enable_remote::Request  &req, drive_values::enable_remote::Response &res);
 
   ros::NodeHandle nh_;
 
-  bool is_remote_driving   = true;
-  bool is_allowed_to_drive = false;
+//   bool is_remote_driving   = true;
+//   bool is_allowed_to_drive = false;
+	bool is_deadman_enabled;
 
   int linear_, angular_;
   double l_scale_, a_scale_;
-  ros::Publisher vel_pub_;
+  ros::Publisher vel_pub_, deadman_pub_;
   ros::Subscriber joy_sub_, states_sub_;
 
+  ros::ServiceServer driving_enabled_srv;
+
+  static bool triggered_driving_enabled_change(drive_values::enable_remote::Request  &req, drive_values::enable_remote::Response &res) {
+	driving_enabled = req.enabled;
+	res.current_state = driving_enabled;
+	return true;
+}
 };
 
 
@@ -40,15 +53,20 @@ RemoteDrive::RemoteDrive():
     // Setup subscribing to joystick topic
     joy_sub_ = nh_.subscribe<sensor_msgs::Joy>("joy", 10, &RemoteDrive::joyCallback, this);
 
+	deadman_pub_ = nh_.advertise<std_msgs::Bool>("drive_values/deadman_state", 10);
+
+    // Setup service
+    driving_enabled_srv = nh_.advertiseService("drive_values/enable_remote", triggered_driving_enabled_change);
+
     // Setup subscribing to global_controller/states topic
-    states_sub_ = nh_.subscribe<global_controller::controller_states>(
-        "/global_controller/states", 10, &RemoteDrive::updateState, this);
+    // states_sub_ = nh_.subscribe<global_controller::controller_states>(
+    //     "/global_controller/states", 10, &RemoteDrive::updateState, this);
 }
 
-void RemoteDrive::updateState(const global_controller::controller_states::ConstPtr& states) {
-    is_remote_driving   = !states->is_driving_automatically;
-    is_allowed_to_drive = states->is_allowed_to_drive;
-}
+// void RemoteDrive::updateState(const global_controller::controller_states::ConstPtr& states) {
+//     is_remote_driving   = !states->is_driving_automatically;
+//     is_allowed_to_drive = states->is_allowed_to_drive;
+// }
 
 void RemoteDrive::joyCallback(const sensor_msgs::Joy::ConstPtr& joy)
 {
@@ -61,14 +79,20 @@ void RemoteDrive::joyCallback(const sensor_msgs::Joy::ConstPtr& joy)
 
 	// Turbo speed when L2 is pressed
 	if (joy->buttons[6] == 1) {
-		turboFactor = 4;
+		turboFactor = 6;
  	}
 
     twist.angular.z = a_scale_*joy->axes[angular_]*turboFactor;
     twist.linear.x = l_scale_*joy->axes[linear_]*turboFactor;
 
+
+	is_deadman_enabled = (joy->buttons[7] == 1) ? true : false;
+	std_msgs::Bool deadmanMsg;
+	deadmanMsg.data = is_deadman_enabled; 
+	deadman_pub_.publish(deadmanMsg);
+
     // Only do remote driving if the remote_driving state is true
-    if (is_remote_driving && is_allowed_to_drive) {
+    if (driving_enabled && is_deadman_enabled) {
         vel_pub_.publish(twist);
     }
 }
@@ -76,9 +100,9 @@ void RemoteDrive::joyCallback(const sensor_msgs::Joy::ConstPtr& joy)
 
 int main(int argc, char** argv)
 {
-  ros::init(argc, argv, "drive_values");
-  RemoteDrive drive_values;
+	ros::init(argc, argv, "drive_values");
+	RemoteDrive drive_values;
 
-  ros::spin();
+	ros::spin();
 }
  
