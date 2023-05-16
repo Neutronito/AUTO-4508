07500877ac4fdeeb5dad9258004259d36b3b5f9f
Neutronito <vg.sinalco@gmail.com>
2023-05-16 15:04:47 +0800

    Lidar Code notifies if obstacle

--- catkin_ws/src/lidar_calcs/src/lidar_response.cpp
+++ catkin_ws/src/lidar_calcs/src/lidar_response.cpp
@@ -1,6 +1,7 @@
 #include "ros/ros.h"
 #include "sensor_msgs/LaserScan.h"
 #include "std_msgs/Int16.h"
+#include <math.h>
 
 ros::Publisher lidar_pub;
 
@@ -9,17 +10,17 @@ void lidarCallback(const sensor_msgs::LaserScan::ConstPtr& msg)
     std_msgs::Int16 lidar_response;
     lidar_response.data = 0;
 
-    //lidar range 0-25, the ranges maps them to 0-100, so in ranges 4 = 1m
-    int thres = 8;          //threshold set to 2m
+    float thres = 1.f;          //threshold set to 1m
     int size = msg->ranges.size();
-    
+
     //Start points for the 3 thirds of the lidar
     int mid = size/2;
-    int left_start = mid - 30;
-    int left_end = mid - 10;
-    int right_start = mid + 10;
-    int right_end = mid + 30;
-
+    float angle_increment = 180*(msg->angle_increment)/M_PI;
+    int left_start = mid - abs(30/angle_increment);
+    int left_end = mid - abs(10/angle_increment);
+    int right_start = mid + abs(10/angle_increment);
+    int right_end = mid + abs(30/angle_increment);
+    int count = 0;
 
     //Check the left third
     for(int i = left_start; i < left_end; i++)
@@ -50,7 +51,8 @@ void lidarCallback(const sensor_msgs::LaserScan::ConstPtr& msg)
             break;
         }
     }
-
+    
+    // 1 obstacle on right, -1 obstacle on left, 2 obstacle in front, 0 no obstacle wooohoooo
     lidar_pub.publish(lidar_response);
 }
 
